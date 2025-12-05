849784e9aeb517c2d0fa34e83909334466e93e40
Ludson Almeida <ludson.bsa@gmail.com>
2025-12-05 13:47:04 -0300

    sdapok n njui

---
+++ next.config.js
@@ -0,0 +1,28 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV !== 'production';
+
+const nextConfig = {
+  // Proxy só em desenvolvimento
+  async rewrites() {
+    if (!isDev) return [];
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://api2.sobradinhoporks.com.br/:path*',
+      },
+    ];
+  },
+
+  // Permite carregar imagens hospedadas na API
+  images: {
+    remotePatterns: [
+      // produção
+      { protocol: 'https', hostname: 'api.sobradinhoporks.com.br' },
+      // dev/local
+      { protocol: 'http', hostname: 'localhost', port: '4000' },
+    ],
+  },
+};
+
+module.exports = nextConfig;
